2c5010895ae352d7d72bb7b293f9bae6f8785451
Gabriel Bolonhez <gbbolonhez@gmail.com>
2025-10-23 23:34:41 -0300

    first commit

---
+++ Context/ScheduleContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_project.Models;
+
+namespace mvc_project.Context
+{
+    public class ScheduleContext : DbContext
+    {
+        public ScheduleContext(DbContextOptions<ScheduleContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace mvc_project.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public bool Active { get; set; }
+    }
+}
